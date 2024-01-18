d0fe1dfd3b12caeb543300ebed0ed0163ca3fecd
Andrea Magnanini <63046209+AndreaMagnanini@users.noreply.github.com>
2024-01-18 15:05:57 +0100

    added term factory

---
+++ Coding Exercise Andrea Magnanini/ITermFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Coding_Exercise_Andrea_Magnanini
+{
+    using Terms;
+
+    public interface ITermFactory
+    {
+        ITerm CreateTerm(string match);
+    }
+}
---
+++ Coding Exercise Andrea Magnanini/TermFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Coding_Exercise_Andrea_Magnanini
+{
+    using Extensions;
+    using Terms;
+
+    public class TermFactory : ITermFactory
+    {
+        public ITerm CreateTerm(string match)
+        {
+            return double.TryParse(match, out var value)
+                ? new SignedNumber(value)
+                : new Operator(match.ToOperatorType());
+        }
+    }
+}
