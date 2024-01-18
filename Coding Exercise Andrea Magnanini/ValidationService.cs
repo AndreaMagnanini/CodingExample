cb101523dee7095d81ff3a076f49a3be7e4ce814
Andrea Magnanini <63046209+AndreaMagnanini@users.noreply.github.com>
2024-01-18 15:06:27 +0100

    added validation service

---
+++ Coding Exercise Andrea Magnanini/ValidationService.cs
@@ -0,0 +1,35 @@
+﻿namespace Coding_Exercise_Andrea_Magnanini
+{
+    using System.Data;
+    using System.Text.RegularExpressions;
+    using Terms;
+
+    public class ValidationService
+    {
+        private const string TermExtractionPattern = @"^\s*([+-]?\d+[.,]?\d*)?\s*([-+xX*/\\])\s*([+-]?\d+[,.]?\d*)\s*$";
+        private readonly ITermFactory termFactory;
+
+        public ValidationService(ITermFactory termFactory)
+        {
+            this.termFactory = termFactory;
+        }
+        
+        public IList<ITerm> BuildExpression(string input)
+        {
+            var correctInput = input.Replace(" ", string.Empty);
+            if (!Regex.IsMatch(correctInput, TermExtractionPattern))
+            {
+                throw new InvalidExpressionException("Invalid Expression.");
+            }
+
+            var matches = Regex.Matches(correctInput, TermExtractionPattern)
+                .First().Groups.Values
+                .ToList();
+            return matches
+                .Skip(1)
+                .Where(m => !string.IsNullOrEmpty(m.Value))
+                .Select(group => this.termFactory.CreateTerm(group.Value))
+                .ToList();
+        }
+    }
+}
