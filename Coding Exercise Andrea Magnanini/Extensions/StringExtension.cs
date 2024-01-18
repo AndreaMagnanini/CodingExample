bb6a6eeda136e51c468edc6bae5d16dce001924d
Andrea Magnanini <63046209+AndreaMagnanini@users.noreply.github.com>
2024-01-18 15:06:10 +0100

    added string to operator conversion

---
+++ Coding Exercise Andrea Magnanini/Extensions/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace Coding_Exercise_Andrea_Magnanini.Extensions
+{
+    using Enums;
+    using System.ComponentModel;
+
+    public static class StringExtension
+    {
+        public static OperatorTypeEnum ToOperatorType(this string character) => character switch
+        {
+            "+" => OperatorTypeEnum.Sum,
+            "-" => OperatorTypeEnum.Sub,
+            "*" or "x" or "X" => OperatorTypeEnum.Mul,
+            "\\" or "/" => OperatorTypeEnum.Div,
+            _ => throw new InvalidEnumArgumentException($"Unrecognized operator '{character}'.")
+        };
+    }
+}
